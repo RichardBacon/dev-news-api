baec5791a553473481d749e9b9ec40613fddf6aa
RichardJonBacon <richardjohnbacon@gmail.com>
2020-10-24 10:15:48 +0100

    Add error handling controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const {
+  send404,
+  handlePSQLErrors,
+  handleCustomErrors,
+  handleInternalErrors,
+} = require('./controllers/errors.controller');
 
 const app = express();
 
@@ -9,5 +15,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).send('app running');
 });
+
+app.use(send404);
+
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
+app.use(handleInternalErrors);
 
 module.exports = app;
