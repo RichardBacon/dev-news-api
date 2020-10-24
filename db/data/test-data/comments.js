34b72305e6ab26210660beeb00cd4a7671e3a0dc
RichardJonBacon <richardjohnbacon@gmail.com>
2020-10-24 11:58:15 +0100

    Update test data for ease of use

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -2,43 +2,71 @@ module.exports = [
   {
     body:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
-    created_at: 1603468763890,
-    created_by: 'ComicDisaster',
-    post_id: 1,
-  },
-  {
-    body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ex dolor. ',
-    created_at: 1603468995254,
-    created_by: 'ComicDisaster',
-    post_id: 2,
-  },
-  {
-    body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nunc efficitur nisi lobortis tempus sit amet et erat. Aenean sit amet consectetur mauris. ',
-    created_at: 1603468995260,
-    created_by: 'SomethingWillTurnUp',
+    created_at: 1600100000000,
+    created_by: 'username1',
     post_id: 1,
   },
   {
     body:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
-    created_at: 1603468763890,
-    created_by: 'ComicDisaster',
+    created_at: 1600200000000,
+    created_by: 'username2',
+    post_id: 1,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600300000000,
+    created_by: 'username1',
+    post_id: 1,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600400000000,
+    created_by: 'username2',
+    post_id: 1,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600500000000,
+    created_by: 'username1',
+    post_id: 1,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600600000000,
+    created_by: 'username1',
+    post_id: 2,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600700000000,
+    created_by: 'username2',
+    post_id: 2,
+  },
+  {
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600800000000,
+    created_by: 'username1',
     post_id: 3,
   },
   {
     body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ex dolor. ',
-    created_at: 1603468995254,
-    created_by: 'ComicDisaster',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600900000000,
+    created_by: 'username1',
     post_id: 4,
   },
   {
     body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nunc efficitur nisi lobortis tempus sit amet et erat. Aenean sit amet consectetur mauris. ',
-    created_at: 1603468995270,
-    created_by: 'SomethingWillTurnUp',
-    post_id: 1,
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo.',
+    created_at: 1600110000000,
+    created_by: 'username2',
+    post_id: 5,
   },
 ];
