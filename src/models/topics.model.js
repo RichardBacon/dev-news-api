3262e6a8c1b8f08cf48f43cd161034e2c02aa247
RichardJonBacon <richardjohnbacon@gmail.com>
2020-10-27 16:39:03 +0000

    Add post topic method

--- src/models/topics.model.js
+++ src/models/topics.model.js
@@ -1,9 +1,42 @@
 const connection = require('../../db/connection');
 
+const { selectUserByUsername } = require('./users.model');
+
 const selectTopics = () => {
   return connection.select('*').from('topics');
 };
 
+const insertTopic = ({ title, description, username }) => {
+  if (!title || !description || !username) {
+    return Promise.reject({
+      status: 400,
+      msg: 'bad request',
+    });
+  }
+
+  return selectUserByUsername({ username })
+    .catch(() => {
+      return Promise.reject({
+        status: 422,
+        msg: 'user not found',
+      });
+    })
+    .then(() => {
+      return connection
+        .insert({
+          title,
+          description,
+          created_by: username,
+        })
+        .into('topics')
+        .returning('*');
+    })
+    .then((topics) => {
+      return topics[0];
+    });
+};
+
 module.exports = {
   selectTopics,
+  insertTopic,
 };
