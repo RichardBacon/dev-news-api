411054c3c7ebca587865fa2d9d1ded22096dae80
Richard Bacon <richardjohnbacon@gmail.com>
2021-03-15 15:08:35 +0000

    Update development data

--- db/data/development-data/posts.js
+++ db/data/development-data/posts.js
@@ -1,50 +1,163 @@
 module.exports = [
   {
-    title: 'Closure',
+    title: 'post1',
     body:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
-    created_at: 1603468763835,
-    created_by: 'TopCoder',
-    topic: 'JavaScript',
+    created_at: 1600100000000,
+    created_by: 'username1',
+    topic: 'topic1',
   },
   {
-    title: 'Hoisting',
-    body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ex dolor. Aliquam interdum volutpat purus, vitae imperdiet nunc pretium vel. Integer volutpat sapien nibh, ac semper arcu rutrum sit amet. Sed semper nunc turpis, sit amet vulputate elit molestie non.',
-    created_at: 1603468995252,
-    created_by: 'TopCoder',
-    topic: 'JavaScript',
-  },
-  {
-    title: 'Scope',
-    body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nunc efficitur nisi lobortis tempus sit amet et erat. Aenean sit amet consectetur mauris. Nam vitae mauris lorem. Donec nec scelerisque dui. Donec vitae ipsum sit amet dolor elementum viverra quis nec tellus. Curabitur maximus odio eu magna aliquet ornare. Cras quis arcu condimentum, sodales massa mollis, fermentum sem. ',
-    created_at: 1603468995253,
-    created_by: 'DevGuy',
-    topic: 'JavaScript',
-  },
-  {
-    title: 'Box Model',
+    title: 'post2',
     body:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
-    created_at: 1603468763835,
-    created_by: 'StyleKing',
-    topic: 'CSS',
+    created_at: 1600200000000,
+    created_by: 'username1',
+    topic: 'topic1',
   },
   {
-    title: 'Media Queries',
+    title: 'post3',
     body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ex dolor. Aliquam interdum volutpat purus, vitae imperdiet nunc pretium vel. Integer volutpat sapien nibh, ac semper arcu rutrum sit amet. Sed semper nunc turpis, sit amet vulputate elit molestie non.',
-    created_at: 1603468995252,
-    created_by: 'StyleKing',
-    topic: 'CSS',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600300000000,
+    created_by: 'username1',
+    topic: 'topic1',
   },
   {
-    title: 'Cascade',
+    title: 'post4',
     body:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nunc efficitur nisi lobortis tempus sit amet et erat. Aenean sit amet consectetur mauris. Nam vitae mauris lorem. Donec nec scelerisque dui. Donec vitae ipsum sit amet dolor elementum viverra quis nec tellus. Curabitur maximus odio eu magna aliquet ornare. Cras quis arcu condimentum, sodales massa mollis, fermentum sem. ',
-    created_at: 1603468995253,
-    created_by: 'DevGuy',
-    topic: 'CSS',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600400000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post5',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600500000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post6',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600600000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post7',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600700000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post8',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600800000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post9',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1600900000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post10',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601000000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+
+  {
+    title: 'post11',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601100000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post12',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601200000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post13',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601300000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post14',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601400000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post15',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601500000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post16',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601600000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post17',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601700000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post18',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601800000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post19',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1601900000000,
+    created_by: 'username1',
+    topic: 'topic1',
+  },
+  {
+    title: 'post20',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ante et libero vehicula iaculis non a leo. Praesent et massa metus. Morbi ultrices porta est, at gravida risus porttitor non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id blandit metus.',
+    created_at: 1602000000000,
+    created_by: 'username1',
+    topic: 'topic1',
   },
 ];
